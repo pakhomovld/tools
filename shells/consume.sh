ee4a9f1aa4b6c87a4870d03ecd344c39506a2424
Lev <30433605+pakhomovld@users.noreply.github.com>
2020-09-13 15:19:08 +0300

    Update consume.sh

--- shells/consume.sh
+++ shells/consume.sh
@@ -9,18 +9,18 @@ CURRENT_INSTANCES=0
 while (( "$CURRENT_INSTANCES" != "$MAX_INSTANCES" ))
 do
     sleep 5
-    CURRENT_INSTANCES=$(ps ax | grep -v grep | grep -ie ":[0-9]\{2\} php /var/www/scoring_2/artisan consume --persistent" 
+    CURRENT_INSTANCES=$(ps ax | grep -v grep | grep -ie ":[0-9]\{2\} php /var/www/app/artisan consume --persistent" 
 | wc -l)
 
     if (( "$CURRENT_INSTANCES" > "$MAX_INSTANCES" ))
 	then
         DIFF_INSTANCES=$(( $CURRENT_INSTANCES - $MAX_INSTANCES ))
-	PIDS=$(ps -ef | grep -v grep | grep ":[0-9]\{2\} php /var/www/scoring_2/artisan consume --persistent" | awk 
+	PIDS=$(ps -ef | grep -v grep | grep ":[0-9]\{2\} php /var/www/app/artisan consume --persistent" | awk 
 '{print $2}' | head -n $DIFF_INSTANCES)
 	kill -9 $PIDS
 	exit 0;
     elif (( "$CURRENT_INSTANCES" < "$MAX_INSTANCES" ))
 	then
-        php /var/www/scoring_2/artisan consume --persistent &
+        php /var/www/app/artisan consume --persistent &
     fi
 done
